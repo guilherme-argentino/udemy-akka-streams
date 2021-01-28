ddeb435046596654861bada91e933e33ccf48211
Guilherme Luiz Argentino Silva <rrantz@gmail.com>
2021-01-28 17:13:50 -0300

    5. First Principles - explicit first run

--- src/main/scala/part2_primer/FirstPrinciples.scala
+++ src/main/scala/part2_primer/FirstPrinciples.scala
@@ -2,9 +2,19 @@ package part2_primer
 
 import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.{Sink, Source}
 
 object FirstPrinciples extends App {
 
   implicit val system = ActorSystem("FirstPrinciples")
   val materializer = ActorMaterializer()
+
+  // sources
+  val source = Source(1 to 10)
+
+  // sinks
+  val sink = Sink.foreach[Int](println)
+
+  val graph = source.to(sink)
+  graph.run()(materializer)
 }
