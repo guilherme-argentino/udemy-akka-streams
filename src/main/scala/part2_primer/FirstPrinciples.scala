8c33c0a6c36926d549d85c1976f9ffe423cddad3
Guilherme Luiz Argentino Silva <rrantz@gmail.com>
2021-01-28 17:18:12 -0300

    5. First Principles - run with flow

--- src/main/scala/part2_primer/FirstPrinciples.scala
+++ src/main/scala/part2_primer/FirstPrinciples.scala
@@ -2,12 +2,12 @@ package part2_primer
 
 import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
-import akka.stream.scaladsl.{Sink, Source}
+import akka.stream.scaladsl.{Flow, Sink, Source}
 
 object FirstPrinciples extends App {
 
   implicit val system = ActorSystem("FirstPrinciples")
-  implicit val materializer = ActorMaterializer()
+  implicit val materialize: ActorMaterializer = ActorMaterializer()
 
   // sources
   val source = Source(1 to 10)
@@ -16,5 +16,13 @@ object FirstPrinciples extends App {
   val sink = Sink.foreach[Int](println)
 
   val graph = source.to(sink)
-  graph.run()
+//  graph.run()
+
+  // flow transform elements
+  val flow = Flow[Int].map(x => x + 1)
+  val sourceWithFlow = source.via(flow)
+  val flowWithSink = flow.to(sink)
+
+  sourceWithFlow.to(sink).run()
+
 }
