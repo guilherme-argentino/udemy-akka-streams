20d250e691c48b8cc6e653e7589f6a19eb22f88d
Guilherme Luiz Argentino Silva <rrantz@gmail.com>
2021-01-29 12:51:24 -0300

    8. Backpressure - first run

---
+++ src/main/scala/part2_primer/BackpressureBasics.scala
@@ -0,0 +1,22 @@
+package part2_primer
+
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.{Sink, Source}
+
+object BackpressureBasics extends App {
+
+  implicit val system: ActorSystem = ActorSystem("BackpressureBasics")
+  implicit val materialize: ActorMaterializer = ActorMaterializer()
+
+  val fastSource = Source(1 to 1000)
+  val slowSink = Sink.foreach[Int] { x =>
+    // simulate a long processing
+    Thread.sleep(1000)
+    println(s"Sink: $x")
+  }
+
+  fastSource.to(slowSink).run() // fusing?!
+  // not backpressure
+
+}
