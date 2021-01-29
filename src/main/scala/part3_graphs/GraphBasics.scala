7ba6e2c6359d282b2b5291a84137c4225e8541f6
Guilherme Luiz Argentino Silva <rrantz@gmail.com>
2021-01-29 13:25:41 -0300

    9. Intro to Graphs and the Graph DSL - first run

---
+++ src/main/scala/part3_graphs/GraphBasics.scala
@@ -0,0 +1,42 @@
+package part3_graphs
+
+import akka.NotUsed
+import akka.actor.ActorSystem
+import akka.stream.{ActorMaterializer, ClosedShape}
+import akka.stream.scaladsl.{Broadcast, Flow, GraphDSL, RunnableGraph, Sink, Source, Zip}
+
+object GraphBasics extends App {
+
+  implicit val system: ActorSystem = ActorSystem("GraphBasics")
+  implicit val materialize: ActorMaterializer = ActorMaterializer()
+
+  val input = Source(1 to 1000)
+  val incrementer = Flow[Int].map(x => x + 1)
+  val multiplier = Flow[Int].map(x => x * 10)
+  val output = Sink.foreach[(Int, Int)](println)
+
+  // step 1 - setting up the fundamentals for the graph
+  val graph = RunnableGraph.fromGraph(
+    GraphDSL.create() { implicit builder: GraphDSL.Builder[NotUsed] =>
+      import GraphDSL.Implicits._ // brings some nice operators into scope
+
+      // step 2 - add the necessary components of this graph
+      val broadcast = builder.add(Broadcast[Int](2)) // fan-out operator
+      val zip = builder.add(Zip[Int, Int]) // fan-in operator
+
+      // step 3 - tying up the components
+      input ~> broadcast
+
+      broadcast.out(0) ~> incrementer ~> zip.in0
+      broadcast.out(1) ~> multiplier ~> zip.in1
+
+      zip.out ~> output
+
+      // step 4 - return a closed shape
+      ClosedShape
+      // shape
+    } // graph
+  ) // runnable graph
+
+  graph.run() // run the graph and materialize it
+}
