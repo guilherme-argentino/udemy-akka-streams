424ba8f60995df53c65c42602a674de9761571d9
Guilherme Luiz Argentino Silva <rrantz@gmail.com>
2021-01-29 12:40:33 -0300

    7. Operator Fusion and Async Boundaries - final

--- src/main/scala/part2_primer/OperatorFusion.scala
+++ src/main/scala/part2_primer/OperatorFusion.scala
@@ -48,8 +48,24 @@ object OperatorFusion extends App {
   //  simpleSource.via(complexFlow).via(complexFlow2).to(simpleSink).run()
 
   // async boundary
-  simpleSource.via(complexFlow).async // runs on one actor
-    .via(complexFlow2).async // runs on another actor
-    .to(simpleSink) // runs on a third actor
-    .run()
+  //  simpleSource.via(complexFlow).async // runs on one actor
+  //    .via(complexFlow2).async // runs on another actor
+  //    .to(simpleSink) // runs on a third actor
+  //    .run()
+
+  // ordering guarantees
+  Source(1 to 3)
+    .map(element => {
+      println(s"Flow A: $element");
+      element
+    }).async
+    .map(element => {
+      println(s"Flow B: $element");
+      element
+    }).async
+    .map(element => {
+      println(s"Flow C: $element");
+      element
+    }).async
+    .runWith(Sink.ignore)
 }
