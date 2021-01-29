0575a8f6a59fd6cbe97881aadda3d5506470681f
Guilherme Luiz Argentino Silva <rrantz@gmail.com>
2021-01-29 18:33:02 -0300

    12. Graph Materialized Values - final

--- src/main/scala/part3_graphs/GraphMaterializedValues.scala
+++ src/main/scala/part3_graphs/GraphMaterializedValues.scala
@@ -1,7 +1,7 @@
 package part3_graphs
 
 import akka.actor.ActorSystem
-import akka.stream.{ActorMaterializer, SinkShape}
+import akka.stream.{ActorMaterializer, FlowShape, SinkShape}
 import akka.stream.scaladsl.{Broadcast, Flow, GraphDSL, Keep, Sink, Source}
 
 import scala.concurrent.Future
@@ -51,7 +51,32 @@ object GraphMaterializedValues extends App  {
   /**
    * Exercise
    */
-  def enhanceFlow[A, B](flow: Flow[A, B, _]): Flow[A, B, Future[Int]] = ???
+  def enhanceFlow[A, B](flow: Flow[A, B, _]): Flow[A, B, Future[Int]] = {
+    val counterSink = Sink.fold[Int, B](0)((count, _) => count + 1)
+
+    Flow.fromGraph(
+      GraphDSL.create(counterSink) { implicit builder => counterSinkShape =>
+        import GraphDSL.Implicits._
+
+        val broadcast = builder.add(Broadcast[B](2))
+        val originalFlowShape = builder.add(flow)
+
+        originalFlowShape ~> broadcast ~> counterSinkShape
+
+        FlowShape(originalFlowShape.in, broadcast.out(1))
+      }
+    )
+  }
+
+  val simpleSource = Source(1 to 42)
+  val simpleFlow = Flow[Int].map(x => x)
+  val simpleSink = Sink.ignore
+
+  val enhancedFlowCountFuture = simpleSource.viaMat(enhanceFlow(simpleFlow))(Keep.right).toMat(simpleSink)(Keep.left).run()
+  enhancedFlowCountFuture.onComplete {
+    case Success(count) => println(s"$count elements went through the enhanced flow")
+    case _ => println("Something failed")
+  }
 
   /*
     Hint: use a broadcast and Sink.fold
