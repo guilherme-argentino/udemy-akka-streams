1032fd0784f3917b95efeaf3395ca351f7e04e14
Guilherme Luiz Argentino Silva <rrantz@gmail.com>
2021-01-30 17:58:57 -0300

    14. Graph Cycles - first solution

---
+++ src/main/scala/part3_graphs/GraphCycles.scala
@@ -0,0 +1,54 @@
+package part3_graphs
+
+import akka.actor.ActorSystem
+import akka.stream.{ActorMaterializer, ClosedShape}
+import akka.stream.scaladsl.{Flow, GraphDSL, Merge, MergePreferred, RunnableGraph, Source}
+
+object GraphCycles extends App {
+
+  implicit val system: ActorSystem = ActorSystem("GraphCycles")
+  implicit val materialize: ActorMaterializer = ActorMaterializer()
+
+  val accelerator = GraphDSL.create() { implicit builder =>
+    import GraphDSL.Implicits._
+
+    val sourceShape = builder.add(Source(1 to 100))
+    val mergeShape = builder.add(Merge[Int](2))
+    val incrementerShape = builder.add(Flow[Int].map { x =>
+      println(s"Accelerating $x")
+      x + 1
+    })
+
+    sourceShape ~> mergeShape ~> incrementerShape
+                   mergeShape <~ incrementerShape
+
+    ClosedShape
+  }
+
+  //  RunnableGraph.fromGraph(accelerator).run()
+  // graph cycle deadlock!
+
+  /*
+    Solution 1: MergePreferred
+   */
+
+  val actualAccelerator = GraphDSL.create() { implicit builder =>
+    import GraphDSL.Implicits._
+
+    val sourceShape = builder.add(Source(1 to 100))
+    val mergeShape = builder.add(MergePreferred[Int](1))
+    val incrementerShape = builder.add(Flow[Int].map { x =>
+      println(s"Accelerating $x")
+      x + 1
+    })
+
+    sourceShape ~> mergeShape ~> incrementerShape
+    mergeShape.preferred <~ incrementerShape
+
+    ClosedShape
+  }
+
+  RunnableGraph.fromGraph(actualAccelerator).run()
+
+
+}
