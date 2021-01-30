4f9f59340e1e44f3c32def1cc7605792395db95f
Guilherme Luiz Argentino Silva <rrantz@gmail.com>
2021-01-30 18:21:07 -0300

    14. Graph Cycles - final

--- src/main/scala/part3_graphs/GraphCycles.scala
+++ src/main/scala/part3_graphs/GraphCycles.scala
@@ -1,8 +1,8 @@
 package part3_graphs
 
 import akka.actor.ActorSystem
-import akka.stream.{ActorMaterializer, ClosedShape, OverflowStrategy}
-import akka.stream.scaladsl.{Flow, GraphDSL, Merge, MergePreferred, RunnableGraph, Source}
+import akka.stream.{ActorMaterializer, ClosedShape, OverflowStrategy, UniformFanInShape}
+import akka.stream.scaladsl.{Broadcast, Flow, GraphDSL, Merge, MergePreferred, RunnableGraph, Sink, Source, Zip}
 
 object GraphCycles extends App {
 
@@ -71,5 +71,61 @@ object GraphCycles extends App {
     ClosedShape
   }
 
-  RunnableGraph.fromGraph(bufferedRepeater).run()
+  //  RunnableGraph.fromGraph(bufferedRepeater).run()
+
+  /*
+    cycles risk deadlocking
+    - add bounds to the number of elements in the cycle
+
+    boundedness vs liveness
+   */
+
+  /**
+   * Challenge: create a fan-in shape
+   * - two input which will be fed with exactly ONE number ( 1 and 1 )
+   * - output will emit an INFINITE FiBONACCI SEQUENCE based off those 2 numbers
+   * 1, 2, 3, 5, 8 ...
+   *
+   * Hint: Use ZipWith and cycles, MergePreferred
+   */
+
+  val fibonacciGenerator = GraphDSL.create() { implicit builder =>
+    import GraphDSL.Implicits._
+
+    val zip = builder.add(Zip[BigInt, BigInt])
+    val mergePreferred = builder.add(MergePreferred[(BigInt, BigInt)](1))
+    val fiboLogic = builder.add(Flow[(BigInt, BigInt)].map { pair =>
+      val last = pair._1
+      val previous = pair._2
+
+      Thread.sleep(100)
+      (last + previous, last)
+    })
+    val broadcast = builder.add(Broadcast[(BigInt, BigInt)](2))
+    val extractLast = builder.add(Flow[(BigInt, BigInt)].map(_._1))
+
+    zip.out ~> mergePreferred           ~> fiboLogic ~> broadcast ~> extractLast
+               mergePreferred.preferred        <~       broadcast
+
+    UniformFanInShape(extractLast.out, zip.in0, zip.in1)
+  }
+
+  val fiboGraph = RunnableGraph.fromGraph(
+    GraphDSL.create() { implicit builder =>
+      import GraphDSL.Implicits._
+
+      val source1 = builder.add(Source.single[BigInt](1))
+      val source2 = builder.add(Source.single[BigInt](1))
+      val sink = builder.add(Sink.foreach[BigInt](println))
+      val fibo = builder.add(fibonacciGenerator)
+
+      source1 ~> fibo.in(0)
+      source2 ~> fibo.in(1)
+      fibo.out ~> sink
+
+      ClosedShape
+    }
+  )
+
+  fiboGraph.run()
 }
