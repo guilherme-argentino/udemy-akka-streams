66d0d5a5660d05137a11fd188dde32f4ae6dcea7
Guilherme Luiz Argentino Silva <rrantz@gmail.com>
2021-02-03 19:10:18 -0300

    19. Testing Akka Streams - first test

--- src/main/scala/part4_techniques/TestingStreamsSpec.scala
+++ src/main/scala/part4_techniques/TestingStreamsSpec.scala
@@ -2,10 +2,14 @@ package part4_techniques
 
 import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.{Keep, Sink, Source}
 import akka.testkit.TestKit
 import org.scalatest.{BeforeAndAfterAll, WordSpecLike}
 
-class TestingStreamsSpec extends TestKit(ActorSystem("TestingAkkaStreams")
+import scala.concurrent.Await
+import scala.concurrent.duration._
+
+class TestingStreamsSpec extends TestKit(ActorSystem("TestingAkkaStreams"))
   with WordSpecLike
   with BeforeAndAfterAll {
 
@@ -16,6 +20,13 @@ class TestingStreamsSpec extends TestKit(ActorSystem("TestingAkkaStreams")
   "A simple stream" should {
     "satisfy basic assertions" in {
       // describe our test
+
+      val simpleSource = Source(1 to 10)
+      val simpleSink = Sink.fold(0)((a: Int, b: Int) => a + b)
+
+      val sumFuture = simpleSource.toMat(simpleSink)(Keep.right).run()
+      val sum = Await.result(sumFuture, 2 seconds)
+      assert(sum == 55)
     }
   }
 
